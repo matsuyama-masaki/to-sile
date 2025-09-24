25f38072fd8779041e2f9f01f90f8d9d2e451d43
matsuyama-masaki <masaki505@gmail.com>
2025-09-24 21:45:43 +0900

    全体セルフチェック

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,7 @@ module ApplicationHelper
   
   # タイトル表示のメソッド
   def page_title(title = '')
+  # タイトルが空でなければ、「タイトル | To-Sile APP」と表示し、空なら「To-Sile APP」と表示する
   base_title = 'To-Sile APP'
   title.present? ? "#{title} | #{base_title}" : base_title
   end
